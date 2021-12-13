cb5a4c0191b455921774b94be09f2f68e5952bdf
Karun <karunakarhv@gmail.com>
2021-12-13 15:08:56 +1100

    Currency Convertor used forex-api

--- Test-Code/currency-convertor.py
+++ Test-Code/currency-convertor.py
@@ -1,5 +1,19 @@
-from currency_converter import CurrencyConverter
-import datetime
+from decimal import Decimal
+import requests
+from datetime import datetime, date, timedelta
+from forex_python.converter import CurrencyRates
 
-currency = CurrencyConverter()
-print(currency.convert(1, 'AUD', 'INR'))
+urlLatest = 'https://theforexapi.com/api/latest'
+
+def getLastSevenDays():
+    dateDict = requests.get(url=urlLatest)
+    dateList = (dateDict.json())['date'].split('-')
+    intDateList = [int(i) for i in dateList]
+    c = CurrencyRates()
+    # Last 7 Days
+    for i in range(7):
+        dateTime =  date(intDateList[0], intDateList[1], intDateList[2]) - timedelta(days=i)
+        print(c.convert('AUD', 'INR', amount=1, date_obj=datetime(dateTime.year, dateTime.month, dateTime.day)))
+
+if __name__ == '__main__':
+    getLastSevenDays()
