7b9e389d2aee08a4e142ce8e4d0d34ec14bc2c5c
Karun <karunakarhv@gmail.com>
2022-01-05 08:49:10 +1100

    Time Diff

---
+++ Test-Code/test-timediff.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+
+def timeDiff(dateTime1, dateTime2):
+    """Finds the difference between two datetime instances.
+
+    Args:
+        dateTime1 (datetime): Start Time
+        dateTime2 (datetime): End Time
+
+    Raises:
+        TypeError: dateTime1 and dateTime2 must be an instance of datetime.
+
+    Returns:
+        datetime: an instance of datetime difference
+    """
+    if dateTime1 and not isinstance(dateTime1, datetime) or \
+            dateTime2 and not isinstance(dateTime2, datetime):
+        raise TypeError("dateTime1 and dateTime2 must be an instance of datetime!")
+    return dateTime1 - dateTime2
+
+class Animal():
+    """[summary]
+    """
+
+startTime = '18:20:50'
+endTime = '18:23:30'
+
+dt1 = datetime.strptime(startTime, '%H:%M:%S')
+dt2 = datetime.strptime(endTime, '%H:%M:%S')
+
+print(timeDiff(dt2, dt1))
+
