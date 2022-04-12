e50973c5e88504df8f475a38add8c5e29e2768be
karunakarhv <karunakarhv@gmail.com>
2022-04-12 10:28:58 +1000

    Added few changes

--- Test-Code/Crontab/crontab-example.py
+++ Test-Code/Crontab/crontab-example.py
@@ -1,7 +1,13 @@
 from crontab import CronTab
 
 cron = CronTab(user='karunakar.venkatesh')
-job = cron.new(command='python3 /home/karunakar.venkatesh/MyPractice/Test-Code/Crontab/example1.py')
-job.minute.every(1)
+job1 = cron.new(command='python3 /home/karunakar.venkatesh/MyPractice/Test-Code/Crontab/example1.py')
+job1.minute.every(1)
+
+job2 = cron.new(command='python3 /home/karunakar.venkatesh/MyPractice/Test-Code/Crontab/example1.py')
+job2.hour.every(2)
+
+for item in cron:
+    print(item)
 
 cron.write()
