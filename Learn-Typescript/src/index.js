20b5a900c210500449e94a99dbd03387f3e656f5
karunakarhv <karunakarhv@gmail.com>
2026-02-19 22:45:27 +1100

    Moved the document to documents folder.

---
+++ Learn-Typescript/src/index.js
@@ -0,0 +1,7 @@
+console.log("Hello World 2");
+var a = 1;
+var b = 2;
+function add(a, b) {
+    return a + b;
+}
+console.log(add(a, b));
---
+++ TypeScript/projects/project1/src/App.tsx
@@ -0,0 +1,13 @@
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+
+function App() {
+  return (
+    <>
+  <Heading title={"Hello"} />
+  <Section title={"Different title"}>This is my section.</Section>
+  </>
+  )
+}
+
+export default App
